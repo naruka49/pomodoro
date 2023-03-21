107ea023b2c4e0632af4173e16ac16146bfe70bd
Naruka <sano0409@gmail.com>
2023-03-21 17:51:58 +0900

    update1

--- main.py
+++ main.py
@@ -72,4 +72,37 @@ def complete_pomodoro():
 # 休憩を完了する関数
 def complete_break():
     status_label.config(text="休憩が終了しました！")
-    start_button.config(state="normal
+    short_break_button.config(state="normal")
+    long_break_button.config(state="normal")
+    reset_button.config(state="normal")
+# タイマーをリセットする関数
+def reset_timer():
+    timer_label.config(text="25:00")
+    status_label.config(text="")
+    start_button.config(state="normal")
+    short_break_button.config(state="normal")
+    long_break_button.config(state="normal")
+    reset_button.config(state="disabled")
+
+#アプリケーションウィンドウを作成する
+    root = tk.Tk()
+    root.title("Pomodoro Timer")
+
+#タイマーと状態のラベルを作成する
+    timer_label = tk.Label(root, text="25:00", font=("Arial", 48))
+    timer_label.pack()
+    status_label = tk.Label(root, text="", font=("Arial", 18))
+    status_label.pack()
+
+#ボタンを作成する
+    start_button = tk.Button(root, text="ポモドーロを開始", command=start_pomodoro)
+    start_button.pack()
+    short_break_button = tk.Button(root, text="短い休憩を開始", command=start_short_break)
+    short_break_button.pack()
+    long_break_button = tk.Button(root, text="長い休憩を開始", command=start_long_break)
+    long_break_button.pack()
+    reset_button = tk.Button(root, text="リセット", command=reset_timer, state="disabled")
+    reset_button.pack()
+
+#アプリケーションウィンドウを表示する
+    root.mainloop()
